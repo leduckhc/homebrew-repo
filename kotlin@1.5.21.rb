2d57a10d4aa4a718482fc1362351d8691e477576
Milan Le <leduc89@gmail.com>
2023-11-20 23:08:26 +0000

    Kotlin: Use official git commit

--- kotlin@1.5.21.rb
+++ kotlin@1.5.21.rb
@@ -1,38 +1,34 @@
-class KotlinAT1521 < Formula
-    desc "Statically typed programming language for the JVM"
-    homepage "https://kotlinlang.org/"
-    url "https://github.com/JetBrains/kotlin/releases/download/v1.5.21/kotlin-compiler-1.5.21.zip"
-    sha256 "f3313afdd6abf1b8c75c6292f4e41f2dbafefc8f6c72762c7ba9b3daeef5da59"
-    license "Apache-2.0"
-    version "1.5.21"
-  
-    livecheck do
-      url :stable
-      regex(/^v?(\d+(?:\.\d+)+)$/i)
-    end
-  
-    bottle do
-      sha256 cellar: :any_skip_relocation, all: "944c2f9b97852160829721ccdf47c510b416454e71f6da0035d9b3d1eac6c50a"
-    end
-  
-    depends_on "openjdk"
-  
-    def install
-      libexec.install "bin", "build.txt", "lib"
-      rm Dir["#{libexec}/bin/*.bat"]
-      bin.install Dir["#{libexec}/bin/*"]
-      bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
-      prefix.install "license"
-    end
-  
-    test do
-      (testpath/"test.kt").write <<~EOS
-        fun main(args: Array<String>) {
-          println("Hello World!")
-        }
-      EOS
-      system "#{bin}/kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
-      system "#{bin}/kotlinc-js", "test.kt", "-output", "test.js"
-      system "#{bin}/kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
-    end
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Promethist/kotlin < Formula
+  desc ""
+  homepage ""
+  url "https://raw.githubusercontent.com/Homebrew/homebrew-core/a1d1a3558543a12aee9d7825029266049ceebb07/Formula/kotlin.rb"
+  version ""
+  sha256 ""
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test promethist/kotlin`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
